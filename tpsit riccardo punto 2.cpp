8f08e157b9807f153569910a8eee6105587cfff6
RiccardoSilvestrini <97669844+RiccardoSilvestrini@users.noreply.github.com>
2022-04-07 12:33:29 +0200

    Add files via upload

---
+++ tpsit riccardo punto 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <time.h>
+
+/* Punto 1 del libro
+- dare la scelta di quanti bits trasmettere (da 8 a 32)
+- generare una sequenza random di bits da trasmettere
+- stampare i dati originali e il polinomio M(x) */
+using namespace std;
+int main() {
+
+int numerobits,c;
+int bit[numerobits];
+cout<<"inserisci quanti bits da trasmettere da (8 a 32): ";
+cin>>numerobits;
+cout<<"numero di bits: "<<numerobits<<endl;
+
+for(int i=0; i<numerobits;i++)
+{ int c=rand() %2;
+cout<<c;
+}
+
+
+int m=4;
+int gx[m];
+gx[0]=1;
+gx[1]=1;
+gx[2]=0;
+gx[3]=1;
+
+cout<<"\n\nmx: ";
+for(int i=m; i>0; i--){
+if (gx[i-1]==1){
+cout<<"x^"<<i-1<<"+";
+}
+}
+
+for(int i=0, h=m; i<numerobits; i++, h--){
+if(h==0){
+h=4;
+}
+if (bit[i] == gx[h]){
+bit[i] = 0;
+}
