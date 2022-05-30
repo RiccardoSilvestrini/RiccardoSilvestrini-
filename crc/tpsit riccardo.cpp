a77803a91e81c4428c3b2bcc620dd7b4074588e4
RiccardoSilvestrini <97669844+RiccardoSilvestrini@users.noreply.github.com>
2022-05-30 14:12:04 +0200

    Update tpsit riccardo.cpp

--- crc/tpsit riccardo.cpp
+++ crc/tpsit riccardo.cpp
@@ -19,168 +19,104 @@
  */
 
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <cstdlib>
-#include <math.h>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
-string diff(string string1, string string2){
-    int n=string1.length();
-    string indicatore= string(n, ' ');
-    for(int i=0; i<n; i++){
-        if(string1[i]==string2[i]){
-            indicatore[i]=' ';
-        }else{
-            indicatore[i]= '^';
-        }
-    }
-    return indicatore;
-}
-
-string corrompi(string nr_mx_corr, int nr_bits_mx, int corr){
-    string ret = nr_mx_corr;
-    for(int i=0; i<corr; i++){
-        int corrotto=rand()%nr_bits_mx;
-        ret[corrotto]= nr_mx_corr[corrotto] == '1' ? '0' : '1';
-    }
-    return ret;
-}
-
-
-string genera_sequenza_bits_da_trasmettere(int nr_bits)
+int main(int argc, char const *argv[])
 {
-    string bits(""); // classe string
-    int random_bit_value = 0;
-    srand(time(NULL));
-    for (int i = 0; i < nr_bits; i++)
-    {
-        random_bit_value = rand() % 2;
-        bits.append(to_string(random_bit_value));
-    }
+    int numBit;
+    string Mx, Gx, Dx, CRC, codeword, newCodeword;
+    bool thereIsErr;
 
-    return bits;
-}
+    getNumBit(numBit);
 
-string divide(int nr_bits_Mx, int g, string Mx_and_zeros, string gen)
-{
-    string temp = Mx_and_zeros;
-    for (int i = 0; i < nr_bits_Mx; i++)
-    {
-        if (gen[0] == temp[i])
-        {
-            for (int j = 0, k = i; j < g + 1; j++, k++)
-            {
-                if (temp[k] == gen[j])
-                    temp[k] = '1';
-                else if (temp[k] != gen[j])
-                    temp[k] = '0';
-            }
-        }
-    }
+    Mx = generateRandomBitString(numBit);
 
-    //TODO migliorare il tutto, questa e' una patch che rimette a posto i bits di MX da trasmettere
-    //for (int i = 0; i < nr_bits_Mx; i++)
-     //   temp[i] = Mx_and_zeros[i];
-    return temp;
-}
+    printCurrentData(numBit, Mx);
 
+    getGx(Gx);
 
+    Dx = generateDx(Mx, Gx.size());
 
+    printDx(Dx);
 
+    CRC = generateCRC(Dx, Gx);
 
-
-int main(int argc, const char *argv[])
-{
-    bool testing = false;
-    // Ema & Amir
-    int nr_bits_da_trasmettere;
-    string scelta;
-    nr_bits_da_trasmettere = 8;
-    cout<<"Inserire bits da 8 a 32: ";
-    cin>>nr_bits_da_trasmettere;
-    if (nr_bits_da_trasmettere > 7 && nr_bits_da_trasmettere < 33)
-        cout << "numero bits da trasmettere: " << nr_bits_da_trasmettere << endl;
-    else
-    {
-        cout << "Numero bits da trasmettere fuori range (8-32): " << nr_bits_da_trasmettere << endl;
-        return 0;
-    }
-    // Amir & Emanuele
-    string Mx("");
-
-    // Emanuele
-
-    if (!testing) {
-        Mx = genera_sequenza_bits_da_trasmettere(nr_bits_da_trasmettere);
-        cout << endl << "generazione random bit da trasmettere ...";
-    } else {   //TODO rimuovere
-        cout << endl << "per test FORZO MX, rimuovere"; 
-        Mx = "11111111";
-        nr_bits_da_trasmettere = Mx.length();
-    }
-    cout << "Bits da trasmettere:\n" << Mx;
-    cout<<endl;
-    string divisore;
-    if (testing) {
-        divisore = "1101";
-    } else {
-        cout << "Inserisci Gx: ";
-        cin >> divisore;
-    }
-    int lunghezza_divisore = divisore.length();
+    printCRC(CRC);
     
-    if (lunghezza_divisore < nr_bits_da_trasmettere - 2) {
-        cout << "Gx valido, corrisponde a " << divisore << " e la sua lunghezza e' " << lunghezza_divisore << " Bits" << endl;
-    } else {
-        cout << "Gx non valido, corrisponde a " << divisore << " e la sua lunghezza e' " << lunghezza_divisore << " Bits, dovrebbe essere massimo " << nr_bits_da_trasmettere - 2 << " Bits" << endl;
-        return 0;
-    }
-    
-    int bits_zero_da_aggiungere_in_coda = lunghezza_divisore + 1;
-    string Mx_with_zeros = Mx + string(bits_zero_da_aggiungere_in_coda, '0'); // oggetto string anonimo, via costruttore di string
+    codeword = generateCodeword(Dx, CRC);
 
-    // Invia
-    string temp = divide(nr_bits_da_trasmettere, lunghezza_divisore, Mx_with_zeros, divisore);
-    // proviamo a estrarre il CRC da sender
-    string CRC = temp.substr(nr_bits_da_trasmettere-1,temp.length()-nr_bits_da_trasmettere);
-    string bits_to_send = Mx+CRC;
-    cout << endl << "bits to send: " << bits_to_send << " CRC: " << CRC;
-    //Ricevi
-    
-    
-    
-    string receiver = divide(nr_bits_da_trasmettere, lunghezza_divisore, Mx_with_zeros, divisore);
-    string CRCv = temp.substr(nr_bits_da_trasmettere-1,temp.length()-nr_bits_da_trasmettere);
-    string bits_verify = Mx+CRCv;
-    cout<<endl<<"Bits to receiver: "<<bits_verify <<" CRC: "<<CRCv;
-    
-    cout<<endl;
-    int corr=0;
-    for(int i=0; i<8+1 ; i++){
-        string Mx_Corrotto = corrompi(Mx, nr_bits_da_trasmettere, corr);
-        string Mx_whit_zero_corrotto = Mx_Corrotto + string(bits_zero_da_aggiungere_in_coda, '0');
-        cout<<endl;
-        cout<<"/------------------/";
-        cout<<endl;
-        cout<<"Corruzione numero "<<i<<endl;
-        string bits_corrotti = divide(nr_bits_da_trasmettere, lunghezza_divisore, Mx_whit_zero_corrotto, divisore);
-        string CRC_corrotto = bits_corrotti.substr(nr_bits_da_trasmettere-1,bits_corrotti.length()-nr_bits_da_trasmettere);
-        string bits_corrotti_uscita = Mx_Corrotto+CRC_corrotto;
-        cout<<"Mx Corrotto"<<endl;
-        cout <<bits_corrotti_uscita<<" CRC "<<CRC_corrotto;
-        string error = diff(Mx, Mx_Corrotto);
-        cout<<endl;
-        cout<<error;
-        cout<<endl;
-        cout<<endl;
-        corr++;
-    }
-    
-    
-    
-    cout<<endl;
+    printCodeword(codeword);
+
+    newCodeword = casualErr(codeword);
+
+    printErr(newCodeword, codeword);
+
+    thereIsErr = checkCodeword(newCodeword, Gx);
+
+    printErrSearch(thereIsErr);
+
+    system("pause");
+
     return 0;
 }
+
+/**
+ * It asks the user to input a number between 8 and 32, and if the number is not between 8 and 32, it
+ * asks the user to input a number between 8 and 32 again
+ * 
+ * @param numBit number of bits to be transmitted
+ */
+void getNumBit(int& numBit) {
+    do {
+        cout << "Inserire quantita' di bit da trasmettere (da 8 a 32): ";
+        cin >> numBit;
+        cout << endl;
+        if(numBit < 8 || numBit > 32) {
+            cout << "La quantita' di bit deve essere compresa tra 8 e 32" << endl << endl;
+        }
+    } while(numBit < 8 || numBit > 32);
+}
+
+/**
+ * It generates a random bit string of a given length
+ * 
+ * @param length the length of the bit string
+ * 
+ * @return A string of random bits.
+ */
+string generateRandomBitString(int length) {
+
+/* La stringa di bit con una lunghezza determinata deve iniziare per forza con 1 
+        altrimenti avrebbe una lunghezza inferiore a quella richiesta */
+    string bitString = "1";
+
+    for(int i = 1; i < length; i++) {
+        bitString += to_string(randomNum(1));
+    }
+
+    return bitString;
+}
+
+/**
+ * It returns a random number between 0 and the number passed in
+ * 
+ * @param max The maximum number that can be generated.
+ * 
+ * @return The remainder of the random number divided by the max number plus 1.
+ */
+int randomNum(int max) {
+    return rand() % (max + 1);
+}
+
+/**
+ * It prints the length of the polynomial and the polynomial itself
+ * 
+ * @param length the length of the string Mx
+ * @param Mx the polynomial
+ */
+void printCurrentData(int length, string Mx) {
+    cout << "Lunghezza M(x): " << length << endl;
+    cout << "M(x): " << Mx 
