79b79b01290fb457b4029fe621d6c8cd85ae7403
RiccardoSilvestrini <97669844+RiccardoSilvestrini@users.noreply.github.com>
2022-11-14 09:33:03 +0100

    Add files via upload

---
+++ fork4.c
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+int main() {
+	int pidl, pid2, pid3;
+	pid1 = fork();
+	pid2 = fork();
+	pid3 = fork();
+	if ((pid1 == 0) || (pid2 == 0) || (pid3 == 0)) {
+		printf("Sono il processo foglio con pid:%d.", getpid());
+		printfn(" il mio papi ha pid. %d\n", getpid());
+		sleep(1);
+		exit(1);
+	}
+	else
+	sleep(2);
+	printf("Sono ilprocesso padre con pid:%d, n\", getpid());
+}
+
+
+   return 0;
+}
